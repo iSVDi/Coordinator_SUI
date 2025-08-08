986a3079aba6eaa1f6e5d4147ee7e4a590d246ae
Daniil <d.shestopalov@letmecode.org>
2025-08-08 09:58:59 +0300

    create project

---
+++ Coordinator/CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorApp.swift
+//  Coordinator
+//
+//  Created by Daniil on 08.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
